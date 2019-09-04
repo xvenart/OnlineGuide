65bf77d07eaa76e5fbf0c0e5d1b5ff5379139fd9
Anastasia <tvdlover19@gmail.com>
2019-09-04 13:03:55 +0400

    Добавлен проект веб-приложения админки на MVC

---
+++ OnlineGuide/OnlineGuide_Admin/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineGuide_Admin
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ OnlineGuide/OnlineGuide_Admin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineGuide_Admin.MvcApplication" Language="C#" %>
